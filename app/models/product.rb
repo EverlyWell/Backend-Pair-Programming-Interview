a928df6711991a7131fed6f487c608ec19b6e685
Greg Answer <greganswer@gmail.com>
2022-11-18 18:54:44 -0500

    Add minimum length validation to models

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,3 @@
 class Product < ApplicationRecord
+  validates_length_of :name, minimum: 5
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,4 @@
 class Question < ApplicationRecord
+  validates_length_of :answer, minimum: 5
+  validates_length_of :label, minimum: 5
 end
