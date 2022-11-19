27d75bd714e80a65af061254736ef36e3f72edc8
Greg Answer <greganswer@gmail.com>
2022-11-19 12:58:55 -0500

    Add validation specs to Product and Question models

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it { should validate_length_of(:name).is_at_least(5) }
+  end
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it { should validate_length_of(:answer).is_at_least(5) }
+    it { should validate_length_of(:label).is_at_least(5) }
+  end
 end
