ffdad20a691263a1a8e515ab2d95e813d188630b
Greg Answer <greganswer@gmail.com>
2022-11-19 17:29:05 -0500

    Add seed Products and questions

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+products = Product.create([{ name: 'T-Shirt' }, { name: 'Pants' }, { name: 'Shoes' }])
+products.each do |product|
+  product.questions.create(label: 'What size is it?', answer: 'Medium')
+end
