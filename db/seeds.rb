6273afa7c228dbb4ec4dbfa57c60c4b4781b2122
Greg Answer <greganswer@gmail.com>
2022-11-19 18:09:32 -0500

    Make seed data more realistic

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,19 @@
-products = Product.create([{ name: 'T-Shirt' }, { name: 'Pants' }, { name: 'Shoes' }])
-products.each do |product|
-  product.questions.create(label: 'What size is it?', answer: 'Medium')
-end
+# Products without questions
+Product.create([{ name: "Women's Health" }, { name: 'Metabolism Test' }])
+
+# General questions
+Question.create(
+  label: 'When do I get results?',
+  answer: 'Results are returned 1-3 days after the lab receives your test sample.'
+)
+Question.create(
+  label: 'What guidance will I get with my results?',
+  answer: 'You will get tailored suggestions on what to do next.'
+)
+
+# Food Sensitivity Test
+product = Product.create(name: 'Food Sensitivity Comprehensive Test')
+product.questions.create(
+  label: 'How does this test work?',
+  answer: 'This test measures how reactive your IgG antibodies are to 204 common foods.'
+)
