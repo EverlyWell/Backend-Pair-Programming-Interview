c2ab64a784cf7566531cc9ee3a3ecb98d4726700
Greg Answer <greganswer@gmail.com>
2022-11-19 13:09:29 -0500

    Add associations specs to ProductQuestion models

--- spec/models/product_question_spec.rb
+++ spec/models/product_question_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe ProductQuestion, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:product_question) { ProductQuestion.new }
+
+  describe "associations" do
+    it { expect(product_question).to belong_to(:product) }
+    it { expect(product_question).to belong_to(:question) }
+  end
 end
