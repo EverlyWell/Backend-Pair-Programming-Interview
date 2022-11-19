cee93e3de38279a8a34caa4b5044175d4b663984
Greg Answer <greganswer@gmail.com>
2022-11-18 19:07:26 -0500

    Add ProductQuestion join table

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,6 @@
 class Product < ApplicationRecord
+  has_many :product_questions, dependent: :destroy
+  has_many :questions, through: :product_questions
+
   validates_length_of :name, minimum: 5
 end
