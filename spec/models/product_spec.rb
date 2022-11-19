d34909773184e9af5948aa5300b444b6543c7cc1
Greg Answer <greganswer@gmail.com>
2022-11-19 13:03:15 -0500

    Add associations specs to Product and Question models

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,6 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
+  let(:product) { Product.new }
+
+  describe "associations" do
+    it { expect(product).to have_many(:product_questions).dependent(:destroy) }
+    it { expect(product).to have_many(:questions).through(:product_questions) }
+  end
+
   describe "validations" do
     it { should validate_length_of(:name).is_at_least(5) }
   end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,6 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
+  let(:question) { Question.new }
+
+  describe "associations" do
+    it { expect(question).to have_many(:product_questions).dependent(:destroy) }
+    it { expect(question).to have_many(:products).through(:product_questions) }
+  end
+
   describe "validations" do
     it { should validate_length_of(:answer).is_at_least(5) }
     it { should validate_length_of(:label).is_at_least(5) }
